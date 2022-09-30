4dca98eb1a8fe81810da80f3c506ab86c78a6ec8
Ana Luisa Pererira Ferro <analuisaferro@gmail.com>
2022-09-30 18:02:01 -0300

    algo novo

--- iluminacao/admin.py
+++ iluminacao/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
+
+admin.site.register(OrdemDeServico)
+admin.site.register(Contribuinte)
+admin.site.register(Bairro)
+
 
 # Register your models here.
