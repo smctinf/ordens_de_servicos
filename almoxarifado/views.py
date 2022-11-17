4a28e652f1cc55d563eaccae5bc0c7e798529b79
Ana Luisa Pererira Ferro <analuisaferro@gmail.com>
2022-11-17 17:01:17 -0300

    modal e mascara

--- almoxarifado/views.py
+++ almoxarifado/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import *
 from .forms import *
+from django.contrib import messages
 
 def index(request):
     materiais=Material.objects.all()
@@ -20,6 +21,7 @@ def adicionar_tipo_materiais(request):
         form=Tipo_Material_Form(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Tipo de material cadastrado!')
             return redirect('alm_listar_tipos')
     else:
         form=Tipo_Material_Form()
@@ -34,6 +36,7 @@ def adicionar_material(request):
         form=Material_Form(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Material cadastrado!')
             return redirect('alm_listar_tipos')
     else:
         form=Material_Form()
@@ -55,6 +58,10 @@ def adicionar_material_ao_estoque(request):
             material.qnt_em_estoque=material.qnt_em_estoque+log.add_quantidade
             material.save()
             log.save()
+            if log.add_quantidade == 1:
+                messages.success(request, f"{log.add_quantidade} nova unidade adicionada ao estoque. Total: {material.qnt_em_estoque}.")
+            else: 
+                messages.success(request, f"{log.add_quantidade} novas unidades adicionadas ao estoque. Total: {material.qnt_em_estoque}.")
             return redirect('alm_index')
     else:
         form=Log_estoque_Form()
